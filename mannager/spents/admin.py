6d462a17a98b0d3d6e840adee929763c94c29b34
Manuel <manuel.andres66.mab@gmail.com>
2023-11-25 16:37:22 -0600

    Account Admin

--- mannager/spents/admin.py
+++ mannager/spents/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Spent, SpentCategory, Earn, EarnCategory
+from .models import Spent, SpentCategory, Earn, EarnCategory, Account
 # Register your models here.
 admin.site.register(Spent)
 admin.site.register(SpentCategory)
 admin.site.register(Earn)
 admin.site.register(EarnCategory)
+admin.site.register(Account)
