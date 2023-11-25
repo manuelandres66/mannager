410f4655ba4d85c4255f78b9f423df4e9e7457ea
Manuel <manuel.andres66.mab@gmail.com>
2023-11-25 16:36:31 -0600

    Account

--- mannager/spents/admin.py
+++ mannager/spents/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Spent, SpentCategory
+from .models import Spent, SpentCategory, Earn, EarnCategory
 # Register your models here.
 admin.site.register(Spent)
 admin.site.register(SpentCategory)
+admin.site.register(Earn)
+admin.site.register(EarnCategory)
--- mannager/spents/models.py
+++ mannager/spents/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 # Create your models here.
 
+class Account(models.Model):
+    name = models.CharField(max_length=250)
+    total = models.FloatField()
+
 class SpentCategory(models.Model):
     name = models.CharField(max_length=250)
     def __str__(self):
@@ -9,5 +13,19 @@ class SpentCategory(models.Model):
 
 class Spent(models.Model):
     money = models.FloatField()
+    name = models.CharField(max_length=250, null=True, blank=True)
+    date = models.DateTimeField()
+    category = models.ForeignKey(SpentCategory, on_delete=models.CASCADE)
+    account = models.ForeignKey(Account, on_delete=models.CASCADE, null=True, blank=True)
+
+class EarnCategory(models.Model):
     name = models.CharField(max_length=250)
-    category = models.ForeignKey(SpentCategory, on_delete=models.CASCADE, null=True, blank=True)
+    def __str__(self):
+        return self.name
+
+class Earn(models.Model):
+    money = models.FloatField()
+    name = models.CharField(max_length=250, null=True, blank=True)
+    date = models.DateTimeField()
+    category = models.ForeignKey(EarnCategory, on_delete=models.CASCADE)
+    account = models.ForeignKey(Account, on_delete=models.CASCADE, null=True, blank=True)
