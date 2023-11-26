0b2694f12ae54220fcdc13cd78cc54b09880937d
manuelandres66 <57593279+manuelandres66@users.noreply.github.com>
2023-11-25 18:21:31 -0600

    Update settings.py

--- mannager/mannager/settings.py
+++ mannager/mannager/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-yde!%ptl%@@t--!uspzivb*6#qkg4%r_^dg-w%+em3knazew=u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
