2af42515d1a4038e44583a01d8fa42c2dd25589d
manuelandres66 <57593279+manuelandres66@users.noreply.github.com>
2023-11-28 09:42:59 -0600

    a

--- mannager/spents/views.py
+++ mannager/spents/views.py
@@ -6,6 +6,6 @@ import requests
 def home(request):
     headers =  {'X-RapidAPI-Key': '3b9b9c776emsh06c4f835f217b1ep1daf80jsn05841d6a2883',
                 'X-RapidAPI-Host': 'currency-exchange.p.rapidapi.com'}
-    dollar = requests.get("https://currency-exchange.p.rapidapi.com/listquotes", headers=headers)
+    dollar = requests.get("https://currency-exchange.p.rapidapi.com/exchange?from='USD'&to='COP'&q='1'", headers=headers)
     print(dollar)
     return HttpResponse(dollar)
