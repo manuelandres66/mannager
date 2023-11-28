dd002b2fdb95827f565529e793796cb4a8c2aa59
manuelandres66 <57593279+manuelandres66@users.noreply.github.com>
2023-11-28 15:57:26 -0600

    AAA

--- mannager/spents/views.py
+++ mannager/spents/views.py
@@ -2,12 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import requests
 from bs4 import BeautifulSoup
+from .models import Account
 
 def update_currency():
     html_content = requests.get("https://www.dolarhoy.co").text
     soup = BeautifulSoup(html_content,"html.parser")
     header_element = soup.select('tr')[17].select('td')[-1].text[2:7]
     DOLLAR = int(header_element.replace(',', ''))
+    for account in Account.objects.all():
+        if account.currency_dollars:
+            account.total_pesos = account.total_dollars * DOLLAR
+        else:
+            account.total_dollars = round(total_pesos / DOLLAR, 2)
     return DOLLAR
 
 # Create your views here.
