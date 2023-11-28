5d0364576e10321a6655a5109bb2d98cdd21aa2a
manuelandres66 <57593279+manuelandres66@users.noreply.github.com>
2023-11-28 09:08:51 -0600

    Dollar

--- mannager/spents/views.py
+++ mannager/spents/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
 
 # Create your views here.
 def home(request):
-    return HttpResponse('hola')
+    dollar = requests.get("https://api.exchangeratesapi.io/v1/latest?access_key=8a920d3e399466722e755da29ad02a18&base=COP&symbols=USA")
+    print(dollar)
+    return HttpResponse(dollar)
