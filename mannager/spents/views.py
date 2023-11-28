ac5cefec21e9fd9f4b32b219e22e83b391b2db15
manuelandres66 <57593279+manuelandres66@users.noreply.github.com>
2023-11-28 12:19:02 -0600

    Making it fuction

--- mannager/spents/views.py
+++ mannager/spents/views.py
@@ -3,10 +3,13 @@ from django.http import HttpResponse
 import requests
 from bs4 import BeautifulSoup
 
-# Create your views here.
-def home(request):
+def update_currency():
     html_content = requests.get("https://www.dolarhoy.co").text
     soup = BeautifulSoup(html_content,"html.parser")
     header_element = soup.select('tr')[17].select('td')[-1].text[2:7]
     DOLLAR = int(header_element.replace(',', ''))
-    return HttpResponse(DOLLAR)
+    return DOLLAR
+
+# Create your views here.
+def home(request):
+    return HttpResponse(update_currency())
