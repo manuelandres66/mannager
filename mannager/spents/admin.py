0d5b640604e3c9dd198e206f5b7df27643ca5b1d
manuelandres66 <57593279+manuelandres66@users.noreply.github.com>
2023-12-01 17:01:17 -0600

    a

--- mannager/spents/admin.py
+++ mannager/spents/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from .models import Spent, SpentCategory, Earn, EarnCategory, Account, SubCash
 # Register your models here.
+
+class SubCashAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Book._meta.get_fields()]
+
+
+admin.site.register(Book, BookAdmin)
 admin.site.register(Spent)
 admin.site.register(SpentCategory)
 admin.site.register(Earn)
 admin.site.register(EarnCategory)
 admin.site.register(Account)
-admin.site.register(SubCash)
+admin.site.register(SubCash, SubCashAdmin)
