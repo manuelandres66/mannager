c6e05653cff566ae69540311c83df08e27b86ebf
manuelandres66 <57593279+manuelandres66@users.noreply.github.com>
2023-12-01 17:03:06 -0600

    a

--- mannager/spents/admin.py
+++ mannager/spents/admin.py
@@ -6,7 +6,6 @@ class SubCashAdmin(admin.ModelAdmin):
     list_display = [field.name for field in SubCash._meta.get_fields()]
 
 
-admin.site.register(Book, BookAdmin)
 admin.site.register(Spent)
 admin.site.register(SpentCategory)
 admin.site.register(Earn)
