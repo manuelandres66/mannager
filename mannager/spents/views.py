54bf57192ed35aa7609505627aece0bae311b189
manuelandres66 <57593279+manuelandres66@users.noreply.github.com>
2023-12-04 16:45:12 -0600

    a

--- mannager/spents/views.py
+++ mannager/spents/views.py
@@ -179,7 +179,7 @@ def edit(request):
 def get_earn(request):
     one_day = datetime.datetime.now() - datetime.timedelta(days=1)
     print(one_day)
-    one_day_obj = Earn.objects.filter(data__gte=one_day, date__lt=timezone.now())
+    one_day_obj = Earn.objects.filter(date__gte=one_day, date__lt=timezone.now())
     print(one_day_obj.values())
 
 def home(request):
